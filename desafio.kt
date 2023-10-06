efac3987d6d4601d396e9d2a67bdeaba46d0e62b
Juan Carlo <134108261+Juancwb@users.noreply.github.com>
2023-10-06 16:07:20 +0000

    Incluido a matricula bem como o curso dos usuarios.

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,50 @@
 // [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
 
-enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
+enum class Nivel { BASICO, INTERMEDIARIO, AVANCADO }
 
-class Usuario
+class Usuario (val nome: String)
 
 data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
 
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
+data class Formacao(val nome: String, val nivel: Nivel, var conteudos: List<ConteudoEducacional>) {
 
     val inscritos = mutableListOf<Usuario>()
     
     fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+        inscritos.add(usuario)
+        println("${usuario.nome} foi matriculado no curso $nome")
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    val conteudo1 = ConteudoEducacional("Introdução ao Kotlin", 90)
+    val conteudo2 = ConteudoEducacional("Programação Orientada a Objetos", 120)
+    val conteudo3 = ConteudoEducacional("Estruturas de Dados", 150)
+    val conteudo4 = ConteudoEducacional("Modelagem de Dados", 90)
+    val conteudo5 = ConteudoEducacional("Algoritimos", 120)
+    val conteudo6 = ConteudoEducacional("CyberSecurity", 60)
+    
+    val formacao1 = Formacao("Desenvolvimento de Software", Nivel.INTERMEDIARIO, listOf(conteudo1, conteudo2, conteudo3))
+    val formacao2 = Formacao("Engenharia de Software", Nivel.AVANCADO, listOf(conteudo1, conteudo2, conteudo3, conteudo4, conteudo5, conteudo6))
+    
+    val usuario1 = Usuario("Alice")
+    val usuario2 = Usuario("Janio")
+    val usuario3 = Usuario("Bento")
+    val usuario4 = Usuario("Ubirajara")
+
+    formacao1.matricular(usuario1)
+    formacao1.matricular(usuario2)
+
+    println("Inscritos na formação ${formacao1.nome}:")
+    for (usuario in formacao1.inscritos) {
+        println(usuario.nome)
+    }
+    
+    formacao2.matricular(usuario3)
+    formacao2.matricular(usuario4)
+    
+    println("Inscritos na formação ${formacao2.nome} :")
+    for (usuario in formacao2.inscritos) {
+        println(usuario.nome)
+    }
 }
